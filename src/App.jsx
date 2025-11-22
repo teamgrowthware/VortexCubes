cd765d81759fd79c072b6e2a3752376ed2bc8426
nisha254 <nishaishwaryadav24@gmail.com>
2025-11-22 11:20:01 +0530

    changes nisha 22 nov

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import Home from './Screen/Home/Home'
+import ExpertiseSection from './Screen/Home/Components/OurExpertise'
+import AlllProjects from './Screen/Home/Components/AlllProjects'
+import RecentWorks from './Screen/Home/Components/RecentWorks'
 
 
 function App() {
 
   return (
     <>
+    <div className='bg-black'>
      <Home/>
+    
+     <AlllProjects/>
+     <div className=' p-8'>
+      <ExpertiseSection/>
+     <RecentWorks/>
+     </div>
+     </div>
     </>
   )
 }
